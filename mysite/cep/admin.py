99aefedc6cdaa98453694dca2089cf53d1d492ee
talhamasood0000 <64506978+talhamasood0000@users.noreply.github.com>
2022-04-10 02:27:59 +0500

    Added Migrations

--- mysite/cep/admin.py
+++ mysite/cep/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import MyUser,Guest,Members,InternationalChapter,CoreBody,ExecutiveMembers,GeneralMember,Activities,Inventory,Manages
+
+admin.site.register(MyUser)
+admin.site.register(Guest)
+admin.site.register(Members)
+admin.site.register(InternationalChapter)
+admin.site.register(CoreBody)
+admin.site.register(ExecutiveMembers)
+admin.site.register(GeneralMember)
+admin.site.register(Activities)
+admin.site.register(Inventory)
+admin.site.register(Manages)
