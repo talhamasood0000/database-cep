2384afac348c10795751603c0a36ea34c3ce5eb5
talhamasood0000 <64506978+talhamasood0000@users.noreply.github.com>
2022-04-10 02:32:50 +0500

    Added remaining models to admin

--- mysite/cep/admin.py
+++ mysite/cep/admin.py
@@ -1,15 +1,21 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import MyUser,Guest,Members,InternationalChapter,CoreBody,ExecutiveMembers,GeneralMember,Activities,Inventory,Manages
+from . import models
 
-admin.site.register(MyUser)
-admin.site.register(Guest)
-admin.site.register(Members)
-admin.site.register(InternationalChapter)
-admin.site.register(CoreBody)
-admin.site.register(ExecutiveMembers)
-admin.site.register(GeneralMember)
-admin.site.register(Activities)
-admin.site.register(Inventory)
-admin.site.register(Manages)
+admin.site.register(models.MyUser)
+admin.site.register(models.Guest)
+admin.site.register(models.Members)
+admin.site.register(models.InternationalChapter)
+admin.site.register(models.CoreBody)
+admin.site.register(models.ExecutiveMembers)
+admin.site.register(models.GeneralMember)
+admin.site.register(models.Activities)
+admin.site.register(models.Inventory)
+admin.site.register(models.Manages)
+admin.site.register(models.Team)
+admin.site.register(models.PartOf)
+admin.site.register(models.Reports)
+admin.site.register(models.Views)
+admin.site.register(models.Supervises)
+admin.site.register(models.RequestView)
