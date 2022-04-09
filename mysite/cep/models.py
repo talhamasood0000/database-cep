4da31cdcd7348c6c1159e3e0339dcc4a3bcfa01e
talhamasood0000 <64506978+talhamasood0000@users.noreply.github.com>
2022-04-09 21:14:53 +0500

    Added one model

--- mysite/cep/models.py
+++ mysite/cep/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 
 # Create your models here.
+
+class InternationalChapter(models.Model):
+    yp_id=models.IntegerField(primary_key=True)
+    region_no=models.IntegerField(max_length=3)
+
+
