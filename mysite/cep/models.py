31cdaeec6846fcc1fec1847f31dfb0f1cfa9d0f5
talhamasood0000 <64506978+talhamasood0000@users.noreply.github.com>
2022-04-13 22:01:08 +0500

    Added data

--- mysite/cep/models.py
+++ mysite/cep/models.py
@@ -1,50 +1,64 @@
-from ast import Mod
+from email.policy import default
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 from django.contrib.auth.models import User
-from numpy import tri
+from django.utils import timezone
 
 
 # Create your models here.
 
 class MyUser(models.Model):
-    id=models.IntegerField(primary_key=True)
+    id=models.AutoField(primary_key=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     department = models.CharField(max_length=100,null=True)
     phonenumber=models.CharField(max_length=20,null=True)
-
+    
+    def __str__(self):
+        return self.user.username
 
 class Guest(models.Model):
     name=models.ForeignKey(MyUser,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
 
 class Members(models.Model):
     name=models.ForeignKey(MyUser,on_delete=models.CASCADE)
     type=models.CharField(max_length=100,null=False)
 
+    def __str__(self):
+        return self.name.user.username
+
 class InternationalChapter(models.Model):
-    yp_id=models.IntegerField(primary_key=True)
+    yp_id=models.AutoField(primary_key=True)
     region_no=models.IntegerField()
+    
+
 
 class CoreBody(models.Model):
-    id=models.IntegerField(primary_key=True)
+    id=models.AutoField(primary_key=True)
     user=models.ForeignKey(Members,on_delete=models.CASCADE)
     post=models.CharField(max_length=100,null=False)
     duties=models.CharField(max_length=100,null=False)
     ypid=models.ForeignKey(InternationalChapter,on_delete=models.CASCADE)
 
+    def __str__(self):
+        return self.post
+
 class ExecutiveMembers(models.Model):
     user=models.ForeignKey(Members,on_delete=models.CASCADE)
 
-
-
+    def __str__(self):
+        return self.user.name.user.username
 
 class GeneralMember(models.Model):
     user=models.ForeignKey(Members,on_delete=models.CASCADE)
     duties=models.CharField(max_length=100,null=True)
 
+    def __str__(self):
+        return self.user.user.name.user.username
 
 class Activities(models.Model):
-    event_id=models.IntegerField(primary_key=True)
+    event_id=models.AutoField(primary_key=True)
     registration_fee=models.IntegerField()
     venue=models.CharField(max_length=100,null=False)
     date_time=models.DateTimeField(null=False)
@@ -52,12 +66,15 @@ class Activities(models.Model):
     member_id=models.ForeignKey(ExecutiveMembers,on_delete=models.CASCADE)
 
 class Inventory(models.Model):
-    item_id=models.IntegerField(primary_key=True)
+    item_id=models.AutoField(primary_key=True)
     name=models.CharField(max_length=100,null=False)
     description=models.CharField(max_length=100,null=False)
     quantity=models.IntegerField()
-    issue=models.DateTimeField(null=False)
+    issue=models.DateTimeField(null=True,blank=True)
     type=models.CharField(max_length=100,null=False)
+
+    def __str__(self):
+        return self.name
 
 class Manages(models.Model):
     item_id=models.ForeignKey(Inventory,on_delete=models.CASCADE)
@@ -71,13 +88,12 @@ class Team(models.Model):
     member_id=models.ForeignKey(GeneralMember,on_delete=models.CASCADE)
     event_id=models.ForeignKey(Activities,on_delete=models.CASCADE)
 
-
 class PartOf(models.Model):
     member_id=models.ForeignKey(Members,on_delete=models.CASCADE)
     name=models.ForeignKey(Team,on_delete=models.CASCADE)
 
 class Reports(models.Model):
-    report_id=models.IntegerField(primary_key=True)
+    report_id=models.AutoField(primary_key=True)
     no_of_attendees=models.IntegerField()
     proceedings=models.CharField(max_length=100,null=False)
     event_id=models.ForeignKey(Activities,on_delete=models.CASCADE)
@@ -90,11 +106,14 @@ class Supervises(models.Model):
     report_id=models.ForeignKey(Activities,on_delete=models.CASCADE)
     international_member=models.ForeignKey(CoreBody,on_delete=models.CASCADE)
 
-
 class RequestView(models.Model):
-    request_date=models.CharField(max_length=100,null=False,primary_key=True)
-    issue_date=models.DateTimeField(null=False)
+    id=models.AutoField(primary_key=True)
+    issue_date=models.DateTimeField(default=timezone.now)
     return_date=models.DateTimeField(null=False)
     item_id=models.ForeignKey(Inventory,on_delete=models.CASCADE)
     int_mem_id=models.ForeignKey(Members,on_delete=models.CASCADE)
 
+    def __str__(self):
+        return str(self.item_id.name)+str(self.int_mem_id.name.user.username)
+
+        
