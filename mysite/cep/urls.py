4099483fb76774bce7bd91e894a634d9631f6a29
talhamasood0000 <64506978+talhamasood0000@users.noreply.github.com>
2022-04-17 21:03:31 +0500

    Added all views

--- mysite/cep/urls.py
+++ mysite/cep/urls.py
@@ -5,4 +5,9 @@ from . import views
 urlpatterns = [
    path('',views.index,name='index'),
    path('aboutus/',views.aboutus,name='aboutus'),
+   path('contact/',views.contact,name='contact'),
+   path('teachers/',views.teachers,name='teachers'),
+   path('courses/',views.courses,name='courses'),
+   path('blogsingle/',views.blogsingle,name='blogsingle'),
+   path('blog/',views.blog,name='blog'),
 ] 
--- mysite/cep/views.py
+++ mysite/cep/views.py
@@ -4,11 +4,22 @@ from .models import *
 # Create your views here.
 
 def index(request):
-    object=Members.objects.all()
-    
-    context={'hello':object}
-
-    return render(request, 'cep/index.html',context)
+    return render(request, 'cep/index.html')
 
 def aboutus(request):
     return render(request, 'cep/about.html')
+
+def contact(request):
+    return render(request, 'cep/contact.html')
+
+def teachers(request):
+    return render(request, 'cep/teachers.html')
+
+def courses(request):
+    return render(request, 'cep/course-grid-2.html')
+
+def blogsingle(request):
+    return render(request, 'cep/blog-single.html')
+
+def blog(request):
+    return render(request, 'cep/blog.html')
