91aa243c83c3150fec7880465def9ce312526544
talhamasood0000 <64506978+talhamasood0000@users.noreply.github.com>
2022-05-01 14:50:53 +0500

    Login System Under Process

--- mysite/cep/models.py
+++ mysite/cep/models.py
@@ -2,7 +2,8 @@ from django.urls import reverse
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
-
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Create your models here.
 
@@ -14,6 +15,15 @@ class MyUser(models.Model):
     
     def __str__(self):
         return self.user.username
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        MyUser.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
 
 class Guest(models.Model):
     name=models.ForeignKey(MyUser,on_delete=models.CASCADE)
